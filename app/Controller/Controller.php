274d65c9b6a7f18fb54a7dc1d92cab686a65cfb2
Germano Corrêa <germanobruscato@gmail.com>
2016-11-17 09:55:36 -0200

    Pequenas modificações da Controller

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -9,17 +9,11 @@
 namespace Controller;
 
 
-/**
- * Class Controller
- * @package Controller
- */
 abstract class Controller
 {
-    /**
-     * @var array
-     * Variables that will be passed to the visualization
-     */
     protected $variables;
+
+    protected $model;
 
     public function __construct()
     {
@@ -28,29 +22,17 @@ abstract class Controller
         $this->defineUserMenu($logged);
     }
 
-    /**
-     * @param mixed $var
-     * @param mixed $value
-     * Set a determined variable in the array of variables
-     */
     public function setVariables($var, $value)
     {
         $this->variables[$var] = $value;
     }
 
-    /**
-     * @return object
-     * Return an object with the variables, to be used later in the visualization
-     */
+
     public function getVariables()
     {
         return (object) $this->variables;
     }
 
-    /**
-     * @param object $model
-     * Still don't know shit
-     */
     public function setModel($model)
     {
         // $model_name = get_class($model);
